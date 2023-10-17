e3c043a7f40d1267155963d791a94bb4da9eb8f0
TrentBrunson <bbiz2@icloud.com>
2023-10-17 09:30:07 -0500

    dimension reduction

--- attrits.R
+++ attrits.R
@@ -1,1 +1,17 @@
-# examine attriion by team, leader and group by month
+# examine attrition by team, leader and group by month
+
+# load packages
+library(tidyverse)
+library(cowplot)
+library(dplyr)
+library(scales)
+
+# load data
+terminations <- read_csv("data\\terminations.csv")
+terminations
+
+# select columns
+colnames(terminations)
+headers <- c{"Termination Month", "L6 Mgr Name"}
+terminations_reduced <- terminations |> select(headers)
+terminations_reduced
