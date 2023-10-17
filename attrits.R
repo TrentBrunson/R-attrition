c97c730d0274e4dfc85ced6741ec24d48bf876c1
TrentBrunson <bbiz2@icloud.com>
2023-10-17 10:57:54 -0500

    refactored

--- attrits.R
+++ attrits.R
@@ -13,13 +13,16 @@ terminations
 # select columns
 colnames(terminations)
 headers <- c("Employee ID", "Termination Month", "L6 Mgr Name")
-terminations_reduced <- terminations |> select(all_of(headers), 
-  as.factor(terminations$"Termination Month", terminations$"L6 Mgr Name"))
+terminations_reduced <- terminations |> select(all_of(headers))
 
 # terminations_reduced <- terminations |> select(all_of(headers), 
   # as.factor(terminations$"Termination Month", terminations$"L6 Mgr Name"))
-  
+
+# change all character columns to factors
+terminations_reduced <- as.data.frame(unclass(terminations_reduced), stringsAsFactors=TRUE)
 terminations_reduced
+
+str(terminations_reduced)
 
 # change data types to factor for chr
 ggplot(terminations_reduced, aes(x = "L6 Mgr Name", y = "Employee ID")) +
