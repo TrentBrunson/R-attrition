341b8be88f0ffcf21bd5d72eff6f55c2c699a19e
TrentBrunson <bbiz2@icloud.com>
2023-10-30 17:51:32 -0500

    colored bar chart

--- attrits.R
+++ attrits.R
@@ -18,28 +18,36 @@ colnames(terminations)
 
 terminations$month
 
+L6s <- c("Smith, Brandee", 
+         "Heeney, Mark", 
+         "Guffey, Roger",
+         "Bouillion, Gary",
+         "Carle, Mindy",
+         "Williams, Danielle",
+         "Alamuru, Chai")
+  
+unique(terminations$L6.Mgr.Name)
+L6s
+str(L6s)
 
 # change all character columns to factors
 terminations <- as.data.frame(unclass(terminations), stringsAsFactors=TRUE)
 terminations
 
-
-# all GOPO terminations
-ggplot(terminations, aes(x = month, y = "Employee ID")) +
-  geom_col()
-
 # terminations by L6
 # drop rows of non-GOPO
-terminations |>
-  filter()
-ggplot(terminations, aes(x = L6.Mgr.Name, y = "Employee ID")) +
-  geom_col()
+terminations <- terminations |> filter(L6.Mgr.Name %in% L6s)
+terminations
 
-ggplot(terminations, aes(x = "Employee ID")) +  geom_bar()
+unique(terminations$L6.Mgr.Name)
 
+# all GOPO terminations
+ggplot(terminations, aes(x = month, fill = L6.Mgr.Name)) +
+  geom_bar() 
+  
 # select columns
 colnames(terminations)
-headers <- c("Employee ID", "Termination Month", "L6 Mgr Name")
+headers <- c("Employee.ID", "Termination.Month", "L6.Mgr.Name")
 terminations_reduced <- terminations |> select(all_of(headers))
 
 # terminations_reduced <- terminations |> select(all_of(headers), 
@@ -52,30 +60,6 @@ terminations_reduced
 str(terminations_reduced)
 
 # change data types to factor for chr
-ggplot(terminations_reduced, aes(x = "L6 Mgr Name", y = "Employee ID")) +
-  geom_bar(
-    stat = "identity"
-  )
-
-
-
-
-ggplot(terminations_reduced, aes(x = "Termination Month", y = "L6 Mgr Name")) +
-  geom_col()
-
-ggplot(terminations_reduced, aes(x = "Termination Month")) +
+ggplot(terminations_reduced, aes(x = "L6 Mgr Name")) +
   geom_bar()
 
-ggplot(terminations_reduced, aes(x = "Termination Month")) +
-  stat_count(geom = "line", aes(y = after_stat(count), group = 1))
-
-ggplot(terminations_reduced, aes(x = "Termination Month")) +
-  stat_count(aes(y = after_stat(count), group = 1)) +
-  geom_line()
-
-terminations_reduced |> 
-  count("Termination Month") |> 
-  ggplot(aes(x = "Termination Month")) +
-  geom_line()
-
-ggplot(terminations_reduced, aes(x = "Termination Month")) + geom_bar()
